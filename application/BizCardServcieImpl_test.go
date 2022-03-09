965bdb37dab087ac03311155a6a4cdfbb2637926
tae2089 <tae2089002@gmail.com>
2022-03-09 16:58:13 +0900

    #3 TEST reginster bizcard

---
+++ application/BizCardServcieImpl_test.go
@@ -0,0 +1,20 @@
+package application_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"main/application"
+	domain "main/domain"
+	"testing"
+)
+
+func TestBizCardServiceImpl_RegisterBizCard(t *testing.T) {
+	bizCardDto := domain.BizCardDto{
+		Email:       "tae2089",
+		Name:        "taebin",
+		PhoneNumber: "010-xxxx-xxxx",
+		Age:         25,
+	}
+	a := application.BizCardServiceImpl{}
+	result := a.RegisterBizCard(bizCardDto)
+	assert.Equal(t, "tae2089", result.Email())
+}
